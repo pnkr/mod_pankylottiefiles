34f8209b6a6ccd627861b25b4014408bdd54e7b0
Panayiotis Kiriakopoulos <kiriakopoulos_p@hotmail.com>
2022-03-25 14:07:54 +0200

    commited 25-3-2022

--- tmpl/default.php
+++ tmpl/default.php
@@ -13,19 +13,41 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
 
-$modId = 'mod_pankylottiefiles' . $module->id;
+$modId 	= 'mod_pankylottiefiles' . $module->id;
+
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->registerScript('pankylottiefiles.script', 'https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js');
+$wa->useScript('pankylottiefiles.script');
 
 if ($params->get('backgroundimage'))
 {
-	/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
-	$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+
 	$wa->addInlineStyle('
 #' . $modId . '{background-image: url("' . Uri::root(true) . '/' . HTMLHelper::_('cleanImageURL', $params->get('backgroundimage'))->url . '");}
 ', ['name' => $modId]);
-}
+};
 
 ?>
 
 <div id="<?php echo $modId; ?>" class="mod_pankylottiefiles">
+<lottie-player 
+	src="<?php echo $params->get('jsonlottie'); ?>"  
+	background="<?php echo $params->get('backgroundcolor','transparent'); ?>"  
+	speed="<?php echo $params->get('speed','1'); ?>"  
+	style="width: <?php echo $params->get('width'); ?>; height: <?php echo $params->get('height'); ?>;"  
+	<?php 
+		if($params->get('loop')){
+			echo " loop ";
+		};
+		if($params->get('controls')){
+			echo " controls ";
+		};
+		if($params->get('autoplay')){
+			echo " autoplay ";
+		};
+	?>
+>
+</lottie-player>
 	<?php echo $module->content; ?>
 </div>
